dcc56a7ea2371f872b2a6059dcc1b92516ddc9cf
WatcharapolxFa <63010870@kmitl.ac.th>
2023-12-10 20:42:44 +0700

    feat: update

---
+++ first_app/lib/gradient_container.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 16, 2, 80),
+            Color.fromARGB(255, 13, 10, 104)
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello word! ',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+  }
+}
