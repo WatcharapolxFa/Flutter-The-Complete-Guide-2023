6d1eb794a6801669908b1fd6aa60af84d3b344b7
watcharaopol.yot <watcharaopol.yot@1000320010140-0>
2024-01-03 10:13:51 +0000

    feat: 125 of 303 complete.

---
+++ expense_tracker/lib/widget/expenses.dart
@@ -0,0 +1,41 @@
+import 'package:expense_tracker/widget/expenses_list/expenses_list.dart';
+import 'package:expense_tracker/models/expenses.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'New Shoes',
+        amount: 39.99,
+        date: DateTime.now(),
+        category: Category.leisure),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('The chart'),
+          Expanded(
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
