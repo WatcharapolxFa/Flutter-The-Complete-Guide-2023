f66f03144214be8ad35680db149d7f0bc7487898
watcharaopol.yot <watcharaopol.yot@1000320010140-0>
2024-01-10 09:45:46 +0000

    feat: review 68  of 89 complete.

--- adv_basics/lib/start_screen.dart
+++ adv_basics/lib/start_screen.dart
@@ -1,35 +1,38 @@
 import 'package:flutter/material.dart';
-class StartScreen extends StatelessWidget{
-  const StartScreen({super.key});
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+  final void Function() startQuiz;
   @override
-  Widget build(BuildContext context){
-    return  Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset('assets/images/quiz-logo.png',
+  Widget build(BuildContext context) {
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/quiz-logo.png',
           height: 300,
           width: 300,
           color: const Color.fromARGB(150, 255, 255, 255),
+        ),
+        const SizedBox(height: 80),
+        const Text(
+          'Learn Flutter the fun way! ',
+          style: TextStyle(
+            color: Color.fromARGB(255, 237, 223, 252),
+            fontSize: 24,
           ),
-          const SizedBox(height: 80),
-          const Text(
-            'Learn Flutter the fun way! ',
-            style: TextStyle(
-              color:  Color.fromARGB(255, 237, 223, 252),
-              fontSize: 24,
-            ),
-          ),
-          const SizedBox(height: 30),
-          OutlinedButton.icon(onPressed: (){}, 
+        ),
+        const SizedBox(height: 30),
+        OutlinedButton.icon(
+          onPressed: startQuiz,
           style: OutlinedButton.styleFrom(
             foregroundColor: const Color.fromARGB(255, 237, 223, 252),
-
           ),
           icon: const Icon(Icons.arrow_right_alt),
-          label: const Text('Start Quiz'),)
-        ],
-      )
-      );
+          label: const Text('Start Quiz'),
+        )
+      ],
+    ));
   }
 }
