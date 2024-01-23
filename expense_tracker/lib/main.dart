2e7d8bd3db9743d839df84e7dc5aecd1a3d9d593
watcharaopol.yot <watcharaopol.yot@1000320010140-0>
2024-01-23 10:21:51 +0000

    feat: 153 of 304 complete.

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,8 +1,36 @@
 import 'package:expense_tracker/widget/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Color.fromARGB(255, 28, 2, 77),
+);
 void main() {
   runApp(
-    MaterialApp(theme: ThemeData(useMaterial3: true), home: const Expenses()),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.primaryContainer,
+          foregroundColor: kColorScheme.onPrimaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+              ),
+            ),
+      ),
+      home: const Expenses(),
+    ),
   );
 }
