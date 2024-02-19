8218de0ba7dcd6f67eb6f198050f9ff040183403
watcharaopol.yot <watcharaopol.yot@1000320010140-0>
2024-02-19 04:12:01 +0000

    feat:new file

--- internals/android/app/src/main/kotlin/com/example/internals/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.internals
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- leran_debugging/debug_app/lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-import 'package:debug_app/quiz.dart';
-
-void main() {
-  runApp(const Quiz());
-}
